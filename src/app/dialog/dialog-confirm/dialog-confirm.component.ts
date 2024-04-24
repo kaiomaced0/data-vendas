4a407c04358a3fbc887236e5dc4532670e37d478
kaiomaced0 <kaiomm2000@gmail.com>
2024-04-23 22:49:53 -0300

    c confirmdialog add

---
+++ src/app/dialog/dialog-confirm/dialog-confirm.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dialog-confirm',
+  standalone: true,
+  imports: [],
+  templateUrl: './dialog-confirm.component.html',
+  styleUrl: './dialog-confirm.component.css'
+})
+export class DialogConfirmComponent {
+
+}
