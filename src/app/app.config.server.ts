b28d9e3cb22b6e757e74f320ef96657cea7c4ef9
05562849259 <kaiomm2000@gmail.com>
2024-06-12 17:56:48 -0300

    add providers main

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -2,11 +2,25 @@ import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
 import { appConfig } from './app.config';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule, provideHttpClient, withFetch } from '@angular/common/http';
+import { MatNativeDateModule } from '@angular/material/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 const serverConfig: ApplicationConfig = {
   providers: [
     provideServerRendering(),
-    CommonModule
+    CommonModule,
+    provideHttpClient(withFetch()),
+    MatNativeDateModule,
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatDatepickerModule, // Adicione isto
+    BrowserAnimationsModule
   ]
 };
 
