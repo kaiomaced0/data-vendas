aded422e25d12902471ad816bba18af5d24a73a6
kaiomaced0 <kaiomm2000@gmail.com>
2024-09-13 15:06:29 -0300

    estoque puxando dados de getall corretamente

--- src/app/home/estoque/estoque-list/estoque-list.component.ts
+++ src/app/home/estoque/estoque-list/estoque-list.component.ts
@@ -6,11 +6,12 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 import { Produto } from '../../../models/produto.model';
 import { Router } from '@angular/router';
 import { DialogConfirmComponent } from '../../../dialog/dialog-confirm/dialog-confirm.component';
+import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-estoque-list',
   standalone: true,
-  imports: [MatIcon],
+  imports: [MatIcon, MatPaginatorModule],
   templateUrl: './estoque-list.component.html',
   styleUrl: './estoque-list.component.css'
 })
@@ -19,14 +20,26 @@ export class EstoqueListComponent implements OnInit {
   constructor(private router: Router, private produtoService: ProdutoService, private dialog: MatDialog, private snackBar: MatSnackBar) {
   }
 
+  pageSize = 10;
+  page = 1;
+  totalRecords = 0;
   produtos: Produto[] = [];
 
   ngOnInit() {
-    // this.produtoService.list().subscribe((data: Produto[]) => {
-    //   this.produtos = data;
-    // });
+
+    this.produtoService.list(this.page, this.pageSize).subscribe((data: Produto[]) => {
+      this.produtos = data;
+    });
+    this.produtoService.listSize().subscribe((data: number) => {
+      this.totalRecords = data;
+    })
   }
-  editar(id:number) {
+  paginar(event: PageEvent): void {
+    this.page = event.pageIndex + 1;
+    this.pageSize = event.pageSize;
+    this.ngOnInit();
+  }
+  editar(id: number) {
     this.router.navigate([`/produtos/edit/${id}`]);
   }
 
@@ -35,13 +48,14 @@ export class EstoqueListComponent implements OnInit {
   }
 
 
-  deletar(id:number, nome:string){
+  deletar(id: number, nome: string) {
     const dialogRef = this.dialog.open(DialogConfirmComponent, {
       width: '250px',
       data: {
         message: `Tem certeza que deseja Deletar o Produto de nome: ${nome}?`,
         n: nome
-      }});
+      }
+    });
 
     dialogRef.afterClosed().subscribe(result => {
       if (result) {
@@ -60,5 +74,5 @@ export class EstoqueListComponent implements OnInit {
         });
       }
     });
-    }
+  }
 }
