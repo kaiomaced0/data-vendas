1aaf497b81ae1e7173bfd79b9f0e6a2fa8b7260d
05562849259 <kaiomm2000@gmail.com>
2024-10-30 19:52:17 -0300

    alterando new produto

--- src/app/home/produtos/new-produto/new-produto.component.ts
+++ src/app/home/produtos/new-produto/new-produto.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { FormArray, FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormField, MatFormFieldModule, MatLabel } from '@angular/material/form-field';
 import { MatOption, MatSelect, MatSelectModule } from '@angular/material/select';
 import { Router } from '@angular/router';
 import { finalize, Observable } from 'rxjs';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { NgIf } from '@angular/common';
+import { Location, NgFor, NgIf } from '@angular/common';
 import { MatProgressBar } from '@angular/material/progress-bar';
 import { MatButton } from '@angular/material/button';
 import { Produto } from '../../../models/produto.model';
@@ -18,32 +18,68 @@ import { MarcaService } from '../../../services/marca/marca.service';
 @Component({
   selector: 'app-new-produto',
   standalone: true,
-  imports: [FormsModule, MatFormField, MatSelect, MatLabel, MatOption, MatButton, MatSelectModule, ReactiveFormsModule, NgIf, MatProgressBar],
+  imports: [FormsModule, MatFormFieldModule, MatSelect, MatLabel, MatOption, MatButton, MatSelectModule, ReactiveFormsModule, NgIf, MatProgressBar, NgFor],
   templateUrl: './new-produto.component.html',
   styleUrl: './new-produto.component.css'
 })
 export class NewProdutoComponent implements OnInit {
 
-  produto: Produto = new Produto();
-  marcas: Marca[] = [];
   selectedFiles: FileList | null = null;
   img: string[] = [];
   uploadPercent: number | undefined;
-
-  cancelar() {
-    this.router.navigate(['/produto']);
-  }
+  productForm?: FormGroup;
+  productData?: Produto;
+  categories: Categoria[] = [
+    { id: 1, nome: 'Eletrônicos' },
+    { id: 2, nome: 'Roupas' },
+    { id: 3, nome: 'Alimentos' },
+    { id: 4, nome: 'Livros' }
+    // Adicione mais categorias conforme necessário
+  ];
 
   constructor(
     private fb: FormBuilder,
+    private snackBar: MatSnackBar,
     private router: Router,
     private service: ProdutoService,
-    private snackBar: MatSnackBar) {
+    private location: Location
+  ) {}
 
-  }
-  ngOnInit() {
+  ngOnInit(): void {
+    this.productForm = this.fb.group({
+      nome: ['', Validators.required],
+      nomeLongo: ['', Validators.required],
+      descricao: [''],
+      estoque: [, [Validators.required, Validators.min(0)]],
+      valorCompra: [null, [Validators.required, Validators.min(0)]],
+      preco: [null, [Validators.required, Validators.min(0)]],
+      idFornecedor: [null, Validators.required],
+      estoqueMinimo: [null, [Validators.required, Validators.min(0)]],
+      idMarca: [null, Validators.required],
+      codigo: [''],
+      codigoBarras: [''],
+      idCategoria: this.fb.array([], Validators.required),  // Usando FormArray para categorias
+    });
   }
 
+  // Getter para facilitar o acesso ao FormArray de categorias
+  get idCategoria(): FormArray {
+    return this.productForm!.get('idCategoria') as FormArray;
+  }
+
+  // Método para adicionar/remover categoria ao FormArray
+  onCategoryChange(categoryId: number, isChecked: boolean): void {
+    if (isChecked) {
+      this.idCategoria.push(this.fb.control(categoryId));
+    } else {
+      const index = this.idCategoria.controls.findIndex(
+        (control) => control.value === categoryId
+      );
+      if (index >= 0) {
+        this.idCategoria.removeAt(index);
+      }
+    }
+  }
 
   onFileSelected(event: any): void {
     this.selectedFiles = event.target.files;
@@ -57,25 +93,35 @@ export class NewProdutoComponent implements OnInit {
     this.img = this.img.filter(image => image !== imageUrl);
   }
 
+  voltarPagina(): void {
+      this.location.back();
+  }
 
-  adicionarProduto() {
-    // this.produto!.img = this.img;
-    this.service.insert(this.produto!).subscribe({
-      next: (produtoAdicionado) => {
-        console.log('Produto adicionado com sucesso:', produtoAdicionado);
-        this.router.navigate(['/produto']);
+  onSubmit(): void {
+    if (this.productForm!.valid) {
+      this.productData = new Produto(this.productForm!.value);
+      console.log('Dados do Produto:', this.productData);
 
-        this.snackBar.open('Produto adicionado', 'Fechar', {
-          duration: 2000,
-        });
-      },
-      error: (erro) => {
-        console.error('Erro ao adicionar produto:', erro, this.produto!);
-        this.snackBar.open('Erro ao adicionar Produto', 'Fechar', {
-          duration: 2000,
-        });
-      }
-    });
-
+      this.service.insert(this.productData!).subscribe({
+        next: (produtoAdicionado) => {
+          console.log('Produto adicionado com sucesso:', produtoAdicionado);
+          this.router.navigate(['/produto']);
+  
+          this.snackBar.open('Produto adicionado', 'Fechar', {
+            duration: 3000,
+          });
+        },
+        error: (erro) => {
+          console.error('Erro ao adicionar produto:', erro, this.productData!);
+          this.snackBar.open('Erro ao adicionar Produto', 'Fechar', {
+            duration: 3000,
+          });
+        }
+      });
+    } else {
+      this.snackBar.open('Campos não preenchidos!', 'Fechar', {
+        duration: 3000,
+      });
+    }
   }
 }
