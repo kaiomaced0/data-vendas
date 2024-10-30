207f300a7a7a3eb2d35f0517c372ec29f8edbc87
05562849259 <kaiomm2000@gmail.com>
2024-10-30 19:50:48 -0300

    add config provider http fetch

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,10 +1,13 @@
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
 import { appConfig } from './app.config';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
 
 const serverConfig: ApplicationConfig = {
   providers: [
-    provideServerRendering()
+    provideServerRendering(),
+    provideHttpClient(withFetch())
   ]
 };
 
