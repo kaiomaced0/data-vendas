ec08dc4577ed005880cbd0394f428ab6b0371a2a
05562849259 <kaiomm2000@gmail.com>
2024-10-30 19:52:27 -0300

    add constructor produto model

--- src/app/models/produto.model.ts
+++ src/app/models/produto.model.ts
@@ -1,4 +1,5 @@
 import { Categoria } from "./categoria.models";
+import { Marca } from "./marca.models";
 
 export class Produto {
   id?: number;
@@ -7,17 +8,31 @@ export class Produto {
   descricao?: string;
   valorCompra?: string;
   preco?: number;
-  cor?: string;
+  // cor?: string;
   estoque?: number;
   estoqueMinimo?: number;
   img?: string[];
   categorias?: Categoria[];
-  marca?: string;
-  idCor?: number;
+  marca?: Marca;
+  // idCor?: number;
   codigo?: string;
   codigoBarras?: string;
+  idFornecedor?: number;
+  idMarca?: number;
+  idCategoria?: number;
 
-  constructor(
-  ) {
+  constructor(formData: any) {
+    this.nome = formData.nome;
+    this.nomeLongo = formData.nomeLongo;
+    this.descricao = formData.descricao;
+    this.estoque = formData.estoque;
+    this.valorCompra = formData.valorCompra;
+    this.preco = formData.preco;
+    this.idFornecedor = formData.idFornecedor;
+    this.estoqueMinimo = formData.estoqueMinimo;
+    this.idMarca = formData.idMarca;
+    this.codigo = formData.codigo;
+    this.codigoBarras = formData.codigoBarras;
+    this.idCategoria = formData.idCategoria || [];
   }
 }
